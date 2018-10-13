3c8b3daea6ef8bf628333d054123fb1e6bea66f3
DukeChiu <1607010329@s.upc.edu.cn>
2018-10-13 15:47:54 +0800

    for get data

--- get_data/main.py
+++ get_data/main.py
@@ -20,12 +20,11 @@ def get_by_id(list_id):
         sql_line_nav = 'select name from nav'
     elif type(list_id) == list:
         sql_line_nav = 'select name from nav where id in ' + str(tuple(list_id))
-        results = query_select_all(sql_line_nav)
-        name = ''
-        for i in results:
-            name += '`' + i[0] + '`,'
-        name = name[:-1]
-        sql_line_all = 'select ' + name + 'from all_swm'
-        print(sql_line_all)
-        return query_select_all(sql_line_all)
+    results = query_select_all(sql_line_nav)
+    name = ''
+    for i in results:
+        name += '`' + i[0] + '`,'
+    name = name[:-1]
+    sql_line_all = 'select ' + name + 'from all_swm'
+    return query_select_all(sql_line_all)
 
---
+++ sql.py
@@ -0,0 +1,5 @@
+from get_data import main
+main.host = '106.14.9.14'
+main.user = 'dc_user'
+main.pwd = '!($(!))!'
+print(main.get_by_id('all'))
